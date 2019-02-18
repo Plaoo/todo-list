e998e724db005d17e3d02cbe0277ac81080912cd
Paolo Monni <paolo.monni345@gmail.com>
2019-02-18 15:26:21 +0100

    Added rest framework and api directory

--- todo/todolist/models.py
+++ todo/todolist/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class TodoList(models.Model):
     text = models.CharField(max_length=50)
     done = models.BooleanField(default = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.text
+        return f"{self.text} - {self.created_at}"
