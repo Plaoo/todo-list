cbaef20e031bc215ddde13f888eb5ef29ff8507a
Paolo Monni <paolo.monni345@gmail.com>
2019-02-18 14:26:24 +0100

    Fix boolean field

--- todo/todolist/models.py
+++ todo/todolist/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 class TodoList(models.Model):
     text = models.CharField(max_length=50)
-    done = models.Boolean(default = True)
+    done = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.text
