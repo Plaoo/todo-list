40b9de167b15571b4bbaee7aa434a908fef89d28
Paolo Monni <paolo.monni345@gmail.com>
2019-02-19 11:25:30 +0100

    Authentication, only registered users can modify tasks otherwise READONLY

--- todo/todolist/api/views.py
+++ todo/todolist/api/views.py
@@ -1,13 +1,17 @@
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import permissions
 from todolist.models import TodoList
 from todolist.api.serializers import TodoSerializer
+
+
 
 class TodoListCreateAPIView(generics.ListCreateAPIView):
     queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
-    #permission_classes = 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class TodoListDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
