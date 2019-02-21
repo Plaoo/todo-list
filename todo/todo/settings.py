e7a1e6b7cee8baf45d9369e9cb4d42c79afd0185
Paolo Monni <paolo.monni345@gmail.com>
2019-02-21 10:26:19 +0100

    test

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bn(!%75wpd45$glg=id#!arxwf*yj=!gl*4i3-xvex2sflmj0-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todolist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'todo.urls'
 
--- todo/todolist/tests.py
+++ todo/todolist/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import TodoList
+
+class TodoListModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        TodoList.objects.create(text="text test")
+        TodoList.objects.create(done=False)
+
+    def test_text_content(self):
+        todo = TodoList.objects.get(id=1)
+        correct_text = f'{todo.text}'
+        self.assertEquals(correct_text, 'text test')
+    
+    def test_text_content(self):
+        todo = TodoList.objects.get(id=1)
+        correct_status = False
+        self.assertEquals(correct_status, False)
+
